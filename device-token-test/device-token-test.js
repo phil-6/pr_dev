e387bd4aec0b177ed480f73996168b7f1b771c57
Phil Reynolds <github@purpleriver.dev>
2023-05-23 10:48:42 +0100

    test gettin device token

--- device-token-test/device-token-test.js
+++ device-token-test/device-token-test.js
@@ -1,10 +1,14 @@
 // Import the functions you need from the SDKs you need
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-import { getMessaging, getToken } from "https://cdnjs.cloudflare.com/ajax/libs/firebase/9.22.0/firebase-messaging.min.js";
+import {
+    getMessaging,
+    getToken,
+    isSupported
+} from "https://cdnjs.cloudflare.com/ajax/libs/firebase/9.22.0/firebase-messaging.min.js";
 
 // Your web app's Firebase configuration
 
@@ -16,26 +20,47 @@ const firebaseConfig = {
     messagingSenderId: "640602342484",
     appId: "1:640602342484:web:c7cb4c1a3f357854c699c1"
 };
+const FIREBASE_VAPID_KEY = 'BMhhrE5EFPy7C-m038N1e_kwoCabk7ceYZK1ppzZPjorc7nYdfEy7WUyQYr-HTQxZDwyxQShuBvDhtjKbeTpkdw';
+const targetElement = document.getElementById("target")
+let app = null;
+let messaging = null;
 
-
-// Initialize Firebase
-
-const app = initializeApp(firebaseConfig);
-// const messaging = getMessaging(app)
-
-// Get registration token. Initially this makes a network call, once retrieved
-// subsequent calls to getToken will return from cache.
-const messaging = getMessaging(app);
-getToken(messaging, { vapidKey: 'BMhhrE5EFPy7C-m038N1e_kwoCabk7ceYZK1ppzZPjorc7nYdfEy7WUyQYr-HTQxZDwyxQShuBvDhtjKbeTpkdw' }).then((currentToken) => {
-    if (currentToken) {
-        // Send the token to your server and update the UI if necessary
-        console.log(currentToken)
-    } else {
-        // Show permission request UI
-        console.log('No registration token available. Request permission to generate one.');
-        // ...
+const getFirebaseToken = async () => {
+    try {
+        const currentToken = await getToken(messaging, {vapidKey: FIREBASE_VAPID_KEY});
+        if (currentToken) {
+            console.log(currentToken)
+            targetElement.innerHTML = `<h1>Your Device Token</h1>${currentToken}`
+        } else {
+            console.log("No registration token available. Request permission to generate one.");
+            targetElement.innerHTML = `<h1>No registration token available</h1>`
+        }
+    } catch (error) {
+        console.log("An error occurred while retrieving token. ", error);
+        targetElement.innerHTML = `<h1>Something went wrong</h1>${error}`
     }
-}).catch((err) => {
-    console.log('An error occurred while retrieving token. ', err);
-    // ...
-});
+};
+
+const requestForToken = async () => {
+    try {
+        const permission = await Notification.requestPermission();
+        if (permission === "granted") {
+            await getFirebaseToken();
+        }
+    } catch (error) {
+        console.log("An error occurred while getting user permission. ", error);
+        targetElement.innerHTML = `<h1>Something went wrong</h1>${error}`
+    }
+};
+
+const hasFirebaseMessagingSupport = await isSupported();
+if (hasFirebaseMessagingSupport) {
+    console.log("Getting Token")
+    targetElement.innerHTML = `<h1>Getting Token...</h1>`
+    app = initializeApp(firebaseConfig);
+    messaging = getMessaging()
+    await requestForToken();
+} else {
+    console.log("Browser not supported")
+    targetElement.innerHTML = `<h1>Browser not supported</h1>`
+}
